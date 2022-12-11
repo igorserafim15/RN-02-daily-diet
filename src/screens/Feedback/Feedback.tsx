71fe2718a053949e8bad7db9a91803b8d67f6ce1
Igor Serafim <igorserafim2015@hotmail.com>
2022-12-11 18:24:21 -0300

    first commit

---
+++ src/screens/Feedback/Feedback.tsx
@@ -0,0 +1,5 @@
+import { Container } from './styles'
+
+export function Feedback() {
+  return <Container></Container>
+}
---
+++ src/screens/Register/Register.tsx
@@ -0,0 +1,5 @@
+import { Container } from './styles'
+
+export function Register() {
+  return <Container></Container>
+}
